27fd0390e17e6283a55a28635f5ccf5454afc9de
ns319 <77340399+ns319@users.noreply.github.com>
2022-03-30 13:31:47 -0400

    Add number of Items and Jobs deleted

--- Clear_PrintQ.ps1
+++ Clear_PrintQ.ps1
@@ -1,23 +1,33 @@
-# script to delete print queue on server
+# script to clear print queue on supplied host
 
-#if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-#    Start-Process PowerShell.exe -ArgumentList "-NoProfile -FilePath `"$PSCommandPath`"" -Verb RunAs
-#    Exit
-#}
+# elevate to admin
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process PowerShell.exe -ArgumentList "-NoProfile -File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
 
-$server = Read-Host -Prompt 'Enter the server name'
+$HostName = Read-Host -Prompt 'Enter hostname'
+Write-Host ""
 
-Stop-Service -InputObject $(Get-Service -ComputerName $server -Name Spooler)
+Stop-Service -InputObject $(Get-Service -ComputerName $HostName -Name Spooler)
 
-# Give time for spooler service to stop before deleting stuff
-Start-Sleep -Seconds 2
+# give spooler service time to stop before deleting stuff
+Start-Sleep -Seconds 1
 
-Remove-Item -Path \\$server\c$\Windows\System32\spool\PRINTERS\*.shd -Verbose
-Remove-Item -Path \\$server\c$\Windows\System32\spool\PRINTERS\*.spl -Verbose
+# count items, delete everything, then count again and subtract to get how many items deleted
+# each job consists of 2 items, so divide by 2 to show how many jobs deleted
+$Before = (Get-ChildItem -Path \\$HostName\c$\Windows\System32\spool\PRINTERS).count
+$After = (Remove-Item -Path \\$HostName\c$\Windows\System32\spool\PRINTERS\* -Verbose).count
+$Total = ($Before - $After)
+$Jobs = ($Total / 2)
 
-# Give time to delete everything before starting spooler service
-Start-Sleep -Seconds 2
+Write-Host ""
+Write-Host "$Total items ($Jobs jobs) deleted."
 
-Start-Service -InputObject $(Get-Service -ComputerName $server -Name Spooler)
+# wait to make sure we're done deleting everything before starting spooler service
+Start-Sleep -Seconds 1
 
-pause
+Start-Service -InputObject $(Get-Service -ComputerName $HostName -Name Spooler)
+
+Write-Host ""
+Read-Host -Prompt "Press Enter to exit"
